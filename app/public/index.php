baf882c61dd6ec78b07b73b25eee110bd769ce6b
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-12-11 18:29:29 +0200

    keep it up

--- app/public/index.php
+++ app/public/index.php
@@ -1,10 +1,27 @@
 <?php
 // phpinfo();
 
-$pdo = new PDO('mysql:dbname=practizer;host=mysql', 'practizer', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+// $pdo = new PDO('mysql:dbname=practizer;host=mysql', 'practizer', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-$query = $pdo->query('SHOW VARIABLES like "version"');
+// $query = $pdo->query('SHOW VARIABLES like "version"');
 
-$row = $query->fetch();
+// $row = $query->fetch();
 
-echo 'MySQL version:' . $row['Value'];
+// echo 'MySQL version:' . $row['Value'];
+
+if (!isset($_POST['firstname'])) {
+    include  __DIR__ . '/./templates/form.html.php';
+} else {
+    $firstName = $_POST['firstname'];
+    $lastName = $_POST['lastname'];
+
+    if ($firstName == 'Tom' && $lastName == 'Butler') {
+        $output = 'Welcome, oh glorious leader!';
+    } else {
+        $output = 'Welcome to our website, ' .
+      htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
+      htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
+    }
+
+    include  __DIR__ . '/./templates/welcome.html.php';
+}
