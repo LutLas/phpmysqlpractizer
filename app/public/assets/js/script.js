8c873f554b6ae6757abb5d9acbbbf15ab6c182b0
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-12-13 00:02:26 +0200

    asset commiter

---
+++ app/public/assets/js/script.js
@@ -0,0 +1,14 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+var elements = document.querySelectorAll('input[id^="test"]');
+
+for (let ndx = 0; ndx < elements.length; ndx++) {
+  const item = elements[ndx];
+  
+}
+
+// Log to console
+console.log(message)
