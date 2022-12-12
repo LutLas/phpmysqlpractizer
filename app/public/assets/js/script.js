6127e8d41a6873ac0226467c0c584aab86a2a1fa
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-12-13 00:33:51 +0200

    keepitmoving

--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -3,12 +3,14 @@ const message = 'Hello world' // Try edit me
 // Update header text
 document.querySelector('#header').innerHTML = message
 
-var elements = document.querySelectorAll('input[id^="test"]');
+var elements = document.querySelectorAll('input[label^="test"]');
 
 for (let ndx = 0; ndx < elements.length; ndx++) {
   const item = elements[ndx];
-  
-}
 
-// Log to console
-console.log(message)
+  if (item.name.includes("2")) {
+    item.value = 4;
+  }
+  
+  console.log(item.value);
+}
